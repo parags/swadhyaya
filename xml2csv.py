c99d473434b4f7a805ed777505e76cd72b6a6da2
snehalraskar1 <sneha.2424@gmail.com>
2025-03-26 06:39:54 -0400

    Delete xml2csv.py

--- xml2csv.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-path = "/Users/snehalr/Documents/Snehal/Swadhyaya/PyScripts"
-file_path = path + '/export.xml'
-
-df = pd.read_xml(file_path)
-
-df.to_csv(path + '/export.csv', index=False)
