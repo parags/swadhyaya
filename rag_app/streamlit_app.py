fe8c0654a07fab96720be10222b5cbb0fdf32084
Parag Shah <parags@gmail.com>
2025-04-01 13:54:24 -0700

    fixing sqlite3 version issue with pysqlite3-binary

--- rag_app/streamlit_app.py
+++ rag_app/streamlit_app.py
@@ -1,3 +1,7 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import chromadb
 from sentence_transformers import SentenceTransformer
 from fastapi import FastAPI, Query
